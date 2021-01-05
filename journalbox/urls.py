ccceb92469a57f6498194cd12893bbf384ee8c92
Wes <wesleySalesberry@hotmail.com>
2021-01-05 14:34:02 -0500

    fixed the url issue

--- journalbox/urls.py
+++ journalbox/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('authentication.urls')),
+    path('auth/', include('authentication.urls')),
     path('admin/', admin.site.urls),
 ]
