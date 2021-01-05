fe119e0e82846da547988044bae4d2dd9a64ed32
Tim La <email@example.com>
2021-01-05 14:06:34 -0500

    created view for journal page

--- profile_journal/views.py
+++ profile_journal/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
 # Create your views here.
+
+
+class JournalPageView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, "journal_profile.html")
