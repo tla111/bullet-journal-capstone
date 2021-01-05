ec76d8bb42aebecf8fba279e79f6a959f502d7d5
Tim La <email@example.com>
2021-01-05 15:15:56 -0500

    created model and view to show assignments

--- profile_journal/views.py
+++ profile_journal/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, reverse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
+
+from .models import Assignments
 # Create your views here.
 
 
 class JournalPageView(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, "journal_profile.html")
+        kenzie_assignments = Assignments.objects.get(user_created=request.user)
+        return render(request, "journal_profile.html", {'assignments': kenzie_assignments} )
+
