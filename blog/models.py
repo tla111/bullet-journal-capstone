a6e7a9314b17152e933f09dbcb36beffa26eea22
Wes <wesleySalesberry@hotmail.com>
2021-01-07 09:00:33 -0500

    created the blog form to update create and delete blog post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+
+class BlogModel(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    body = models.TextField(max_length=380)
+    tags = models.CharField(max_length=100)
+    list_date = models.DateTimeField(default=timezone.now)
