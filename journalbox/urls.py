057f0db5a4658f97b05b765a496c8426b1f2ae19
svw <stephguirand@aol.com>
2021-01-08 12:51:47 -0500

    added env

--- journalbox/urls.py
+++ journalbox/urls.py
@@ -22,7 +22,6 @@ from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
-    # url(r'^media/(?P.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('auth/', include('authentication.urls')),
     path('journal/', JournalPageView.as_view(), name='journal'),
--- journalbox/wsgi.py
+++ journalbox/wsgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'journalbox.settings')
 
 application = get_wsgi_application()
 
-# project_folder = os.path.expanduser('~/bullet-journal-capstone')  # adjust as appropriate
-# load_dotenv(os.path.join(project_folder, '.env'))
+project_folder = os.path.expanduser('~/bullet-journal-capstone')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
