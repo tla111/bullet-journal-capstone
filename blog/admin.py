7319252b65532239ad79196736154332439fcf51
Wes <wesleySalesberry@hotmail.com>
2021-01-12 12:38:15 -0500

    inital blog with comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import BlogModel, CommentModel
 
-# Register your models here.
+admin.site.register(BlogModel)
+admin.site.register(CommentModel)
--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,15 @@ class BlogModel(models.Model):
     body = models.TextField(max_length=380)
     tags = models.CharField(max_length=100)
     list_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class CommentModel(models.Model):
+    post = models.ForeignKey(
+        BlogModel, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    context = models.TextField(max_length=280)
+    created_date = models.DateTimeField(default=timezone.now)
