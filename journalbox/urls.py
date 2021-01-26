05b814a12f8395798086f05e54b5bf1ecdb60510
Tim La <email@example.com>
2021-01-26 14:27:36 -0500

    uncomment routes

--- journalbox/urls.py
+++ journalbox/urls.py
@@ -23,9 +23,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-    # url(r'^media/(?P<path>.*)$', serve,
-    #     {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}),
     path('auth/', include('authentication.urls')),
     path('journal/', JournalPageView.as_view(), name='journal'),
     path('add_assignment/', AddAssignmentView.as_view()),
