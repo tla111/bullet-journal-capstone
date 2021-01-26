a1a8f19fafa5f680a52b8ba9db276e5ee20a8142
Tim La <email@example.com>
2021-01-26 13:18:26 -0500

    made changes in settings

--- journalbox/settings.py
+++ journalbox/settings.py
@@ -136,10 +136,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
