ade9253d33b4097e0d6fdb64d27c141d5b5e21d8
Tim La <email@example.com>
2021-01-26 13:13:44 -0500

    comment out media

--- journalbox/settings.py
+++ journalbox/settings.py
@@ -136,13 +136,13 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/authentication'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 LOGIN_URL = '/authentication/login/'
