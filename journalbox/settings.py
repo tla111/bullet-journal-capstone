e78faced36b40c2c875b5aea1c0586a6044a1f7a
Tim La <email@example.com>
2021-01-26 13:59:33 -0500

    rearranged lines

--- journalbox/settings.py
+++ journalbox/settings.py
@@ -138,12 +138,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/authentication/login/'
 AUTH_USER_MODEL = "journaluser.BulletJournalUser"
